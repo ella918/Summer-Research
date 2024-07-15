5c1c2314f7951e93961346299f41cc5c09cb53df
Ella Mathews <mathewea@lafayette.edu>
2024-07-15 15:25:43 -0400

    just ids with enough rv

--- GaiaIDList.py
+++ GaiaIDList.py
@@ -1,4 +1,5 @@
-from astropy.table import QTable, vstack
+from astropy.table import QTable, vstack, unique, Table
+import numpy as np
 
 new_6866 = QTable.read('/users/EllaMathews/Research/rcat_ngc6866_v0.fits')
 new_6811 = QTable.read('/users/EllaMathews/Research/rcat_ngc6811_v0.fits')
@@ -7,4 +8,14 @@ new_ids_6866 = new_6866['GAIAEDR3_ID']
 
 id_list = vstack([new_ids_6811, new_ids_6866])
 
-id_list.write("GAIADR3_IDs.csv", format = 'csv', overwrite = True)
+values, counts = np.unique(id_list, return_counts=True)
+id_list_rvs = values[counts >= 3]
+
+idtable = Table()
+ids = []
+for num in id_list_rvs:
+	idnum = np.int64(num)
+	ids.append(idnum)
+idtable["GAIAEDR3_ID"] = ids
+
+idtable.write("GAIADR3_IDs.csv", format = "csv", overwrite = True)
