87ced7d475c2f33f84e5e478470f5b06eb762531
Ella Mathews <mathewea@lafayette.edu>
2024-07-25 15:39:30 -0400

    separating prior generation

---
+++ OnePrior.py
@@ -0,0 +1,66 @@
+#imports
+import astropy.table as at
+import astropy.units as u
+import matplotlib.pyplot as plt
+import numpy as np
+import h5py
+import thejoker as tj
+from astropy.table import QTable, Table, Column, vstack, unique
+from astropy.time import Time
+from astropy.visualization.units import quantity_support
+import astropy.coordinates as coord
+import pymc as pm
+import arviz as az
+import argparse
+import os
+import schwimmbad
+
+
+DATA_PATH = os.getenv("DATA_PATH", "/users/EllaMathews/Summer-Research/") #environment variable 
+jobid = os.getenv("SLURM_JOB_ID", "-9999")
+if jobid != "-9990":
+    workpath = "/scratch/ella_multstar/"
+else:
+    workpath = DATA_PATH
+
+#random generator to ensure reproducibility
+rnd = np.random.default_rng(seed=42)
+
+CreatePriors(num_priors):
+	mils = num_priors/1000000
+
+	prior = tj.JokerPrior.default( #initializing the default prior
+        P_min = 2 * u.day,
+        P_max = 1e3 * u.day,
+        sigma_K0 = 30 * u.km / u.s,
+        sigma_v = 100 * u.km / u.s,
+    )
+    print('initialized default prior')
+
+    if os.path.exists(f'{DATA_PATH}/{mils}M/prior_samples_{mils}M.hdf5') == False:
+    	print('Creating new priors')
+    	prior_samples = prior.sample(size = num_priors, rng = rnd, return_logprobs = True) #generating prior samples
+    	print("New priors created")
+    	prior_samples.write(f"{workpath}/{mils}M/prior_samples_{mils}M.hdf5", overwrite = True) #write out prior samples to research folder 
+    	print("Priors saved to file")
+
+    return
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--prior', help = 'num of prior samp default 50000000', type = int, default = 50000000)
+    args = parser.parse_args()
+    print('args')
+
+    CreatePriors(args.prior)
+
+
+
+
+
+
+
+
+
+
+
