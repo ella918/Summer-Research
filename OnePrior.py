c3dd3b99b9ae45eba22f4d9dc865807cc3b57da9
Ella Mathews <mathewea@lafayette.edu>
2024-07-30 15:52:54 -0400

    changing file name

--- OnePrior.py
+++ OnePrior.py
@@ -36,11 +36,11 @@ def CreatePriors(num_priors):
     )
 	print('initialized default prior')
 
-	if os.path.exists(f'{DATA_PATH}/{mils}M/prior_samples_{mils}M.hdf5') == False:
+	if os.path.exists(f'{DATA_PATH}/{mils}M/prior_samples_{mils:.0f}M.hdf5') == False:
 		print('Creating new priors')
 		prior_samples = prior.sample(size = num_priors, rng = rnd, return_logprobs = True) #generating prior samples
 		print("New priors created")
-		prior_samples.write(f"{workpath}/prior_samples_{mils}M.hdf5", overwrite = True) #write out prior samples to research folder 
+		prior_samples.write(f"{workpath}prior_samples_{mils:.0f}M.hdf5", overwrite = True) #write out prior samples to research folder 
 		print("Priors saved to file!")
 	return
 
