4048003e90f26521296b7b84cbe2afec67f83cef
Ella Mathews <mathewea@lafayette.edu>
2024-07-31 14:32:59 -0400

    debugging error bars

--- UnimodalPlots.py
+++ UnimodalPlots.py
@@ -70,14 +70,15 @@ data_for_plots['e_upper'] = e_upper
 
 
 #plotting e vs P plot with uncertainties 
+fig, ax = plt.subplots()
 x = data_for_plots['P_median']
 y = data_for_plots['e_median']
 asymmetric_error_x = [P_lower, P_upper]
 asymmetric_error_y = [e_lower, e_upper]
-fig, ax = plt.subplots()
 ax.errorbar(x, y, xerr = asymmetric_error_x, yerr = asymmetric_error_y)
 plt.scatter(x, y)
 plt.show()
+plt.savefig('Unimodal_Plots')
 
 
 
