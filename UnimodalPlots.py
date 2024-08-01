b36850453e5da3f6afe0672b0ea97febcae1bb75
Ella Mathews <mathewea@lafayette.edu>
2024-08-01 10:51:16 -0400

    fixing errorbars

--- UnimodalPlots.py
+++ UnimodalPlots.py
@@ -79,10 +79,9 @@ print('made table')
 fig, ax = plt.subplots()
 x = data_for_plots['P_median']
 y = data_for_plots['e_median']
-asymmetric_error_x = [data_for_plots['P_lower'], data_for_plots['P_upper']]
-asymmetric_error_y = [data_for_plots['e_lower'], data_for_plots['e_upper']]
-print(asymmetric_error_y)
-ax.errorbar(x, y, xerr = asymmetric_error_x.all(), yerr = asymmetric_error_y.all())
+#asymmetric_error_x = [data_for_plots['P_lower'], data_for_plots['P_upper']]
+#asymmetric_error_y = [data_for_plots['e_lower'], data_for_plots['e_upper']]
+#ax.errorbar(x, y, xerr = asymmetric_error_x.all(), yerr = asymmetric_error_y.all())
 plt.scatter(x, y)
 plt.show()
 plt.savefig('Unimodal_Plots')
