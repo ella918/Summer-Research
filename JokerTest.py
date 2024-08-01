3cc822f7d775b75f6f9fedc5b511c80a77952081
Ella Mathews <mathewea@lafayette.edu>
2024-08-01 11:56:14 -0400

    testing segmentation error

--- JokerTest.py
+++ JokerTest.py
@@ -1,4 +1,21 @@
 import thejoker as tj
+import astropy.table as at
+import astropy.units as u
+#import matplotlib.pyplot as plt
+# import numpy as np
+# import h5py
+# import thejoker as tj
+# from astropy.table import QTable, Table, Column, vstack, unique
+# from astropy.time import Time
+# from astropy.visualization.units import quantity_support
+# import astropy.coordinates as coord
+# import pymc as pm
+# import arviz as az
+# import argparse
+# import os
+# import schwimmbad
+
+
 samples = tj.JokerSamples.read('prior_samples_50M.hdf5')
 print(samples)
 
