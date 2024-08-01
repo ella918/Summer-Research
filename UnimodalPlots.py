34f7758bc62f7304bf3a9ea61b4273361d2be574
Ella Mathews <mathewea@lafayette.edu>
2024-08-01 11:38:03 -0400

    fixing errorbars

--- UnimodalPlots.py
+++ UnimodalPlots.py
@@ -82,12 +82,12 @@ y = data_for_plots['e_median']
 asymmetric_error_x = np.array([np.array(data_for_plots['P_lower']), np.array(data_for_plots['P_upper'])])
 asymmetric_error_y = np.array([np.array(data_for_plots['e_lower']), np.array(data_for_plots['e_upper'])])
 ax.errorbar(x, y, xerr = asymmetric_error_x, yerr = asymmetric_error_y
-#plt.scatter(x, y)
-#plt.x_label('P (d)')
-#plt.y_label('e')
-#plt.title('e vs P for Unimodal Stars in ngc6811 and ngc6866')
-#plt.show()
-plt.savefig('Unimodal_Plots')
+ax.scatter(x, y)
+ax.x_label('P (d)')
+ax.y_label('e')
+ax.set_title('e vs P for Unimodal Stars in ngc6811 and ngc6866')
+ax.show()
+ax.savefig('Unimodal_Plots')
 print('made plot')
 
 
