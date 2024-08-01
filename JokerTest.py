bbf530db242f57f95c340622044a35c7dbcd70b1
Ella Mathews <mathewea@lafayette.edu>
2024-08-01 11:53:38 -0400

    testing segmentation error

--- JokerTest.py
+++ JokerTest.py
@@ -1,3 +1,10 @@
 import thejoker as tj
 samples = tj.JokerSamples.read('prior_samples_50M.hdf5')
-samples
+print(samples)
+
+prior = tj.JokerPrior.default( #initializing the default prior
+        P_min = 2 * u.day,
+        P_max = 1e3 * u.day,
+        sigma_K0 = 30 * u.km / u.s,
+        sigma_v = 100 * u.km / u.s,
+   )
