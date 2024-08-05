c151e3b0cb492d10b706a01578cf4f69e578d649
Ella Mathews <mathewea@lafayette.edu>
2024-08-05 14:40:01 -0400

    fixing write out error

--- RunTheJokerOnePrior.py
+++ RunTheJokerOnePrior.py
@@ -28,7 +28,8 @@ new_6866 = QTable.read(f'{workpath}/rcat_ngc6866_v0.fits')
 new_6811 = QTable.read(f'{workpath}/rcat_ngc6811_v0.fits')
 
 def RunTheJokerOnePrior(id_num, mpi, num_priors):
-    os.mkdir(f'{workpath}{id_num}')
+    if os.path.exists(f'{workpath}{id_num}') == False:
+        os.mkdir(f'{workpath}{id_num}')
 
     new_ids_6811 = new_6811['GAIAEDR3_ID']
     #print(len(new_ids_6811))
