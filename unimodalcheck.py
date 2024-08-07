a28d2386b11e3dd5f16b185697dce9b989ba8e8f
Ella Mathews <mathewea@lafayette.edu>
2024-08-07 14:22:47 -0400

    checking new data from rerun

--- unimodalcheck.py
+++ unimodalcheck.py
@@ -11,9 +11,11 @@ import os
 
 
 
-workpath = '/data2/labs/douglste-laf-lab/mathewea/TheJoker_Outputs'
+#workpath = '/data2/labs/douglste-laf-lab/mathewea/TheJoker_Outputs'
+workpath = '/data2/labs/douglste-laf-lab/mathewea/50.0M'
 
-idlist = QTable.read('/data2/labs/douglste-laf-lab/mathewea/Summer-Research/GAIADR3_IDs.csv')
+#idlist = QTable.read('/data2/labs/douglste-laf-lab/mathewea/Summer-Research/GAIADR3_IDs.csv')
+idlist = QTable.read('/data2/lab/douglste-laf-lab/mathewea/Summer-Research/rerunids.csv')
 new_6866 = QTable.read('/data2/labs/douglste-laf-lab/mathewea/rcat_ngc6866_v0.fits')
 new_6811 = QTable.read('/data2/labs/douglste-laf-lab/mathewea/rcat_ngc6811_v0.fits')
 
@@ -24,7 +26,7 @@ num_samples = []
 unimodal = []
 #bimodal = []
 
-for idnum in idlist['GAIAEDR3_ID']:
+for idnum in idlist['id']:
 	new_ids_6811 = new_6811['GAIAEDR3_ID']
 	new_ids_6866 = new_6866['GAIAEDR3_ID']
 	datamatched6811 = new_6811[idnum == new_ids_6811]
@@ -33,12 +35,12 @@ for idnum in idlist['GAIAEDR3_ID']:
 	t1 = Time(matched["DATE-OBS"], format = "fits", scale = "tcb")
 	data = tj.RVData(t = t1, rv = matched['vrad']*(u.kilometer/u.second), rv_err = matched['vrad_err']*(u.kilometer/u.second))
 
-	if os.path.exists(f'{workpath}/{idnum}/rejection_samples_{idnum}.hdf5'):
+	if os.path.exists(f'{workpath}/{idnum}/rejection_samples_50.0M/{idnum}.hdf5'):
 		ids.append(idnum)
-		joker_samples = tj.JokerSamples.read(f'{workpath}/{idnum}/rejection_samples_{idnum}.hdf5')
+		joker_samples = tj.JokerSamples.read(f'{workpath}/{idnum}/rejection_samples_50.0M_{idnum}.hdf5')
 		numsamples = len(joker_samples)
 		if os.path.exists(f'{workpath}/{idnum}/rejection_samples_MCMC_{idnum}.hdf5'):
-			joker_samples = tj.JokerSamples.read(f'{workpath}/{idnum}/rejection_samples_MCMC_{idnum}.hdf5')
+			joker_samples = tj.JokerSamples.read(f'{workpath}/{idnum}/rejection_samples_MCMC_50.0M_{idnum}.hdf5')
 			numsamples = 0
 			mcmc_check = 1
 		else:
@@ -65,18 +67,18 @@ datatable['unimodal'] = unimodal
 ReRunTable = Table()
 rerunids = []
 
-datatable.write('unimodalcheck.csv', format = 'csv', overwrite = True)
+datatable.write('unimodalcheck_rerun.csv', format = 'csv', overwrite = True)
 
-for i in range(len(datatable)):
-	if datatable['MCMC'][i] == 0 and datatable['num_samples'][i] < 256:
-		rerunids.append(datatable['id'][i])
-	if datatable['MCMC'][i] == 1 and datatable['unimodal'][i] == 0:
-		rerunids.append(datatable['id'][i])
+# for i in range(len(datatable)):
+# 	if datatable['MCMC'][i] == 0 and datatable['num_samples'][i] < 256:
+# 		rerunids.append(datatable['id'][i])
+# 	if datatable['MCMC'][i] == 1 and datatable['unimodal'][i] == 0:
+# 		rerunids.append(datatable['id'][i])
 
-rerunids.append(2128515216999497600)
-rerunids.append(2128515496174251008)
-rerunids.append(2075875655448389120)
-ReRunTable['id'] = np.unique(rerunids)
+# rerunids.append(2128515216999497600)
+# rerunids.append(2128515496174251008)
+# rerunids.append(2075875655448389120)
+# ReRunTable['id'] = np.unique(rerunids)
 
-ReRunTable.write('rerunids.csv', format = 'csv', overwrite = True)
-print(len(ReRunTable))
+# ReRunTable.write('rerunids.csv', format = 'csv', overwrite = True)
+# print(len(ReRunTable))
