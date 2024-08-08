1e4901917fd5c47eb564209eb146dea80f00ed31
Ella Mathews <mathewea@lafayette.edu>
2024-08-08 15:39:40 -0400

    debugging

--- unimodalcheck.py
+++ unimodalcheck.py
@@ -90,40 +90,40 @@ for idnum in weirdids:
 		unimodal.append(uni)
 		#bimodal.append(bi)
 
-for idnum in idlist2['GAIAEDR3_ID']:
-	if idnum in ids:
-		continue
+# for idnum in idlist2['GAIAEDR3_ID']:
+# 	if idnum in ids:
+# 		continue
 
-	new_ids_6811 = new_6811['GAIAEDR3_ID']
-	new_ids_6866 = new_6866['GAIAEDR3_ID']
-	datamatched6811 = new_6811[idnum == new_ids_6811]
-	datamatched6866 = new_6866[idnum == new_ids_6866]
-	matched = vstack([datamatched6811, datamatched6866])
-	t1 = Time(matched["DATE-OBS"], format = "fits", scale = "tcb")
-	data = tj.RVData(t = t1, rv = matched['vrad']*(u.kilometer/u.second), rv_err = matched['vrad_err']*(u.kilometer/u.second))
+# 	new_ids_6811 = new_6811['GAIAEDR3_ID']
+# 	new_ids_6866 = new_6866['GAIAEDR3_ID']
+# 	datamatched6811 = new_6811[idnum == new_ids_6811]
+# 	datamatched6866 = new_6866[idnum == new_ids_6866]
+# 	matched = vstack([datamatched6811, datamatched6866])
+# 	t1 = Time(matched["DATE-OBS"], format = "fits", scale = "tcb")
+# 	data = tj.RVData(t = t1, rv = matched['vrad']*(u.kilometer/u.second), rv_err = matched['vrad_err']*(u.kilometer/u.second))
 
-	if os.path.exists(f'{workpath2}/{idnum}/rejection_samples_{idnum}.hdf5'):
-		ids.append(idnum)
-		joker_samples = tj.JokerSamples.read(f'{workpath2}/{idnum}/rejection_samples_{idnum}.hdf5')
-		numsamples = len(joker_samples)
-		if os.path.exists(f'{workpath2}/{idnum}/rejection_samples_MCMC_{idnum}.hdf5'):
-			joker_samples = tj.JokerSamples.read(f'{workpath2}/{idnum}/rejection_samples_MCMC_{idnum}.hdf5')
-			numsamples = 0
-			mcmc_check = 1
-		else:
-			mcmc_check = 0
-		if tj.is_P_unimodal(joker_samples, data):
-			uni = 1
-		else:
-			uni = 0
-		#if tj.is_P_Kmodal(joker_samples, data, 2):
-		#	bi = 1
-		#else:
-		#	bi = 0 
-		num_samples.append(numsamples)
-		mcmc.append(mcmc_check)
-		unimodal.append(uni)
-		#bimodal.append(bi)
+# 	if os.path.exists(f'{workpath2}/{idnum}/rejection_samples_{idnum}.hdf5'):
+# 		ids.append(idnum)
+# 		joker_samples = tj.JokerSamples.read(f'{workpath2}/{idnum}/rejection_samples_{idnum}.hdf5')
+# 		numsamples = len(joker_samples)
+# 		if os.path.exists(f'{workpath2}/{idnum}/rejection_samples_MCMC_{idnum}.hdf5'):
+# 			joker_samples = tj.JokerSamples.read(f'{workpath2}/{idnum}/rejection_samples_MCMC_{idnum}.hdf5')
+# 			numsamples = 0
+# 			mcmc_check = 1
+# 		else:
+# 			mcmc_check = 0
+# 		if tj.is_P_unimodal(joker_samples, data):
+# 			uni = 1
+# 		else:
+# 			uni = 0
+# 		#if tj.is_P_Kmodal(joker_samples, data, 2):
+# 		#	bi = 1
+# 		#else:
+# 		#	bi = 0 
+# 		num_samples.append(numsamples)
+# 		mcmc.append(mcmc_check)
+# 		unimodal.append(uni)
+# 		#bimodal.append(bi)
 
 datatable['id'] = ids
 datatable['num_samples'] = num_samples
