808aa1fb74919eb3b9d0335f6d7ea78943aea03f
Ella Mathews <mathewea@lafayette.edu>
2024-08-08 15:06:04 -0400

    fixing unimodal check

--- unimodalcheck.py
+++ unimodalcheck.py
@@ -11,10 +11,10 @@ import os
 
 
 
-#workpath = '/data2/labs/douglste-laf-lab/mathewea/TheJoker_Outputs'
+workpath2 = '/data2/labs/douglste-laf-lab/mathewea/TheJoker_Outputs'
 workpath = '/data2/labs/douglste-laf-lab/mathewea/50.0M'
 
-#idlist = QTable.read('/data2/labs/douglste-laf-lab/mathewea/Summer-Research/GAIADR3_IDs.csv')
+idlist2 = QTable.read('/data2/labs/douglste-laf-lab/mathewea/Summer-Research/GAIADR3_IDs.csv')
 idlist = QTable.read('/data2/labs/douglste-laf-lab/mathewea/Summer-Research/rerunids.csv')
 new_6866 = QTable.read('/data2/labs/douglste-laf-lab/mathewea/rcat_ngc6866_v0.fits')
 new_6811 = QTable.read('/data2/labs/douglste-laf-lab/mathewea/rcat_ngc6811_v0.fits')
@@ -71,7 +71,7 @@ for idnum in weirdids:
 		ids.append(idnum)
 		joker_samples = tj.JokerSamples.read(f'{workpath}/{idnum}/rejection_samples_50.0M_{idnum}.hdf5')
 		numsamples = len(joker_samples)
-		if os.path.exists(f'{workpath}/{idnum}/rejection_samples_MCMC_{idnum}.hdf5'):
+		if os.path.exists(f'{workpath}/{idnum}/rejection_samples_MCMC_50.0M_{idnum}.hdf5'):
 			joker_samples = tj.JokerSamples.read(f'{workpath}/{idnum}/rejection_samples_MCMC_50.0M_{idnum}.hdf5')
 			numsamples = 0
 			mcmc_check = 1
@@ -90,6 +90,40 @@ for idnum in weirdids:
 		unimodal.append(uni)
 		#bimodal.append(bi)
 
+for idnum in idlist2['id']:
+	if idnum in ids:
+		continue
+		
+	new_ids_6811 = new_6811['GAIAEDR3_ID']
+	new_ids_6866 = new_6866['GAIAEDR3_ID']
+	datamatched6811 = new_6811[idnum == new_ids_6811]
+	datamatched6866 = new_6866[idnum == new_ids_6866]
+	matched = vstack([datamatched6811, datamatched6866])
+	t1 = Time(matched["DATE-OBS"], format = "fits", scale = "tcb")
+	data = tj.RVData(t = t1, rv = matched['vrad']*(u.kilometer/u.second), rv_err = matched['vrad_err']*(u.kilometer/u.second))
+
+	if os.path.exists(f'{workpath2}/{idnum}/rejection_samples_{idnum}.hdf5'):
+		ids.append(idnum)
+		joker_samples = tj.JokerSamples.read(f'{workpath2}/{idnum}/rejection_samples_{idnum}.hdf5')
+		numsamples = len(joker_samples)
+		if os.path.exists(f'{workpath2}/{idnum}/rejection_samples_MCMC_{idnum}.hdf5'):
+			joker_samples = tj.JokerSamples.read(f'{workpath2}/{idnum}/rejection_samples_MCMC_{idnum}.hdf5')
+			numsamples = 0
+			mcmc_check = 1
+		else:
+			mcmc_check = 0
+		if tj.is_P_unimodal(joker_samples, data):
+			uni = 1
+		else:
+			uni = 0
+		#if tj.is_P_Kmodal(joker_samples, data, 2):
+		#	bi = 1
+		#else:
+		#	bi = 0 
+		num_samples.append(numsamples)
+		mcmc.append(mcmc_check)
+		unimodal.append(uni)
+		#bimodal.append(bi)
 
 datatable['id'] = ids
 datatable['num_samples'] = num_samples
