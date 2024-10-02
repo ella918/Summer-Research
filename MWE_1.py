74595dbbf8f81152fb5ff2116ae63aeafaf8a215
Ella Mathews <mathewea@lafayette.edu>
2024-10-02 16:00:59 -0400

    50 million samples

--- MWE_1.py
+++ MWE_1.py
@@ -27,7 +27,7 @@ print(dataRV["DATE-OBS"])
 
 id_num = 2128124963389008384
 mpi = True
-num_priors = 50000
+num_priors = 50000000
 
 t1 = Time(dataRV["DATE-OBS"], format = "isot", scale = "tcb")
 data = tj.RVData(t = t1, rv = dataRV['vrad']*(u.kilometer/u.second), rv_err = dataRV['vrad_err']*(u.kilometer/u.second)) 
@@ -73,14 +73,14 @@ if len(joker_samples) == 1:
     mcmc_samples = tj.JokerSamples.from_inference_data(prior, trace, data) #convert trace into jokersamples
     az.summary(trace, var_names=prior.par_names)
     az.plot_trace(trace, var_names = prior.par_names)
-    plt.savefig(f'{DATA_PATH}/traceplot.png')
+    plt.savefig(f'{DATA_PATH}/traceplot_{mils}M_{id_num}.png')
     mcmc_samples.write(f'{DATA_PATH}/rejection_samples_MCMC_{mils}M_{id_num}_MWE.hdf5', overwrite = True) #write out MCMC posterior samples  
 
 
 #PLOTTING THE DATA
 
 rvplotnoline = data.plot()
-plt.savefig(f"{workpath}/RVvTime_{id_num}_MWE")
+plt.savefig(f"{DATA_PATH}/RVvTime_{mils}M_{id_num}_MWE")
 
 #getting the lnk value to put on title
 K = joker_samples['K']
@@ -89,7 +89,7 @@ fig1, ax1 = plt.subplots()
 
 _ = tj.plot_rv_curves(joker_samples, data=data) #plotting RV curves from rejection sampler
 plt.title(f"ID: {id_num}, K1%={K1st}")
-fig1.savefig(f"{workpath}/RVCurves_{id_num}_MWE") #saving figure to plots folder in script output folder
+fig1.savefig(f"{DATA_PATH}/RVCurves_{mils}M_{id_num}_MWE") #saving figure to plots folder in script output folder
 print("RV curves plotted")
 
 #plotting period against eccentricity
@@ -103,7 +103,7 @@ ax2.set_xlabel("$P$ [day]")
 ax2.set_ylabel("$e$")
 plt.title(f"ID: {id_num}, K1%={K1st}")
 
-fig2.savefig(f"{workpath}/PeriodvsEccent_{id_num}_MWE") #saving figure to plots folder in script output  folder 
+fig2.savefig(f"{DATA_PATH}/PeriodvsEccent_{mils}M_{id_num}_MWE") #saving figure to plots folder in script output  folder 
 print("Period vs Eccentricity plotted")
 
 if len(joker_samples) == 1: 
@@ -111,7 +111,7 @@ if len(joker_samples) == 1:
     fig3, ax3 = plt.subplots()
     _ = tj.plot_rv_curves(mcmc_samples, data=data) #plotting RV curves from MCMC rejection sampler
     plt.title(f"ID: {id_num}, K1%={K1st}")
-    fig3.savefig(f"{workpath}/RVCurves_MCMC_{id_num}_MWE") #saving figure to plots folder in script output  folder
+    fig3.savefig(f"{DATA_PATH}/RVCurves_MCMC_{mils}M_{id_num}_MWE") #saving figure to plots folder in script output  folder
     print("RV curves from MCMC plotted")
 
     #plotting period vs eccentricity
@@ -124,6 +124,6 @@ if len(joker_samples) == 1:
     ax4.set_xlabel("$P$ [day]")
     ax4.set_ylabel("$e$")
     plt.title(f"ID: {id_num}, K1%={K1st}")
-    fig4.savefig(f"{workpath}/PeriodvsEccent_MCMC_{id_num}_MWE") #saving figure to plots folder in script output  folder
+    fig4.savefig(f"{DATA_PATH}/PeriodvsEccent_MCMC_{mils}M_{id_num}_MWE") #saving figure to plots folder in script output  folder
     print("Period vs Eccentricity from MCMC plotted")
 plt.close('all')
