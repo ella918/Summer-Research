c24ee94f723e939af8f71f634d1fe8f2908d6efb
Ella Mathews <mathewea@lafayette.edu>
2025-01-02 15:47:42 -0500

    putting line over plot

--- AASunimodal.py
+++ AASunimodal.py
@@ -52,7 +52,9 @@ etest = circ_function(ptest,10.2)
 
 
 
+workpath1 = '/data2/labs/douglste-laf-lab/mathewea/TheJoker_Outputs' #might have to change this 
 workpath2 = '/data2/labs/douglste-laf-lab/mathewea/50.0M'
+unimodal_table1 = Table.read('/data2/labs/douglste-laf-lab/mathewea/Summer-Research/unimodalcheck_olderdata.csv')
 unimodal_table2 = Table.read('/data2/labs/douglste-laf-lab/mathewea/Summer-Research/unimodalcheck_rerun.csv')
 
 data_for_plots = QTable()
@@ -103,6 +105,41 @@ for i in range(len(unimodal_table2)):
         #Map = tj.MAP_sample(joker_samples)
         #MAP.append(Map)
 
+for i in range(len(unimodal_table1)):
+
+    if unimodal_table1['unimodal'][i] == 1:
+        idnum = unimodal_table1['id'][i]
+        ids.append(idnum)
+        if unimodal_table1['MCMC'][i] == 0:
+            joker_samples = tj.JokerSamples.read(f'{workpath1}/{idnum}/rejection_samples_{idnum}.hdf5')
+            MCMC.append(0)
+        if unimodal_table1['MCMC'][i] == 1:
+            joker_samples = tj.JokerSamples.read(f'{workpath1}/{idnum}/rejection_samples_MCMC_{idnum}.hdf5')
+            MCMC.append(1)
+
+        p_median = np.percentile(joker_samples['P'], 50)
+        #print(p_median)
+        p_16 = np.percentile(joker_samples['P'], 16)
+        #print(p_16)
+        p_84 = np.percentile(joker_samples['P'], 84)
+        #print(p_84)
+
+        e_median1 = np.percentile(joker_samples['e'], 50)
+        #print(e_median1)
+        e_16 = np.percentile(joker_samples['e'], 16)
+        #print(e_16)
+        e_84 = np.percentile(joker_samples['e'], 84)
+        #print(e_84)
+
+        P_median.append(p_median)
+        P_lower.append(p_median - p_16)
+        P_upper.append(p_84 - p_median)
+
+        e_median.append(e_median1)
+        e_lower.append(e_median1 - e_16)
+        e_upper.append(e_84 - e_median1)
+
+
 data_for_plots['id'] = ids
 data_for_plots['MCMC'] = MCMC
 data_for_plots['P_median'] = P_median
@@ -113,10 +150,11 @@ data_for_plots['e_lower'] = e_lower
 data_for_plots['e_upper'] = e_upper 
 # #data_for_plots['MAP'] = MAP 
 
-data_for_plots.write('data_for_unimodal_plots_reran.csv', format = 'csv', overwrite = True)
+data_for_plots.write('data_for_unimodal_plots_combined.csv', format = 'csv', overwrite = True)
 print('made table')
 
 
+#plotting e vs P plot with uncertainties 
 plt.figure()
 ax = plt.subplot(111)
 plt.plot(ptest,etest, 'r')
@@ -132,6 +170,6 @@ ax.set_xscale('log')
 ax.set_ylabel('e')
 ax.set_title('e vs P for Unimodal Stars in NGC6811 and NGC6866')
 plt.show()
-plt.savefig('Unimodal_Plots_reran_AAS.png')
+plt.savefig('Unimodal_Plots_combined_AAS.png')
 print('made plot')
 
