10ac4ee2e3f3228d4ef987391fdd81f4d9d3f705
Ella Mathews <mathewea@lafayette.edu>
2025-01-02 16:39:33 -0500

    typo

--- AASplots.py
+++ AASplots.py
@@ -61,7 +61,7 @@ _ = tj.plot_rv_curves(rejectionsamples, data=data)
 ax4.set_title("Possible Orbits")
 print("RV curves after rejection plotted")
 
-plt.subplots_adjust(hspace = 0.8, wspace = 0.8)
+plt.subplots_adjust(hspace = 0.7, wspace = 0.7)
 fig.savefig('plots_AAS.png')
 
 
